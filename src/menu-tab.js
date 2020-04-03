a84bf04f143a34f431ed592dc705a5ee3148fd9b
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-04-03 13:25:02 -0300

    Add some items to restaurant menu list

--- src/menu-tab.js
+++ src/menu-tab.js
@@ -2,11 +2,10 @@ const menuLoad = () => {
   const element = document.getElementById('tab-content');
   element.innerHTML = `<h1>Restaurant menu</h1>
   <ul>
-    <li>MenuItem</li>
-    <li>MenuItem</li>
-    <li>MenuItem</li>
-    <li>MenuItem</li>
-    <li>MenuItem</li>
+    <li>Salmon Pasta - $8</li>
+    <li>Fish Chip Cheese - $22</li>
+    <li>Kerupuk Udang - $15</li>
+    <li>New York Style Pizza - $7</li>
   </ul>`;
 }
 
